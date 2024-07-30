31ee43f7cb70806de609627e39420d2e3dd6d764
Aaron <aaronradlicki@arizona.edu>
2024-07-29 17:23:59 -0700

    Added init.sql file

---
+++ data/init.sql
@@ -0,0 +1,13 @@
+CREATE DATABASE demobahasa;
+ALTER DATABASE demobahasa CHARACTER SET utf8 COLLATE utf8_unicode_ci;
+use demobahasa;
+
+CREATE TABLE lexicon (
+    id INT NOT NULL AUTO_INCREMENT,
+    written_form NVARCHAR(30),
+    origin NVARCHAR(30),
+    surface_ipa NVARCHAR(50),
+    senses ????????
+    surface_simple NVARCHAR(30)
+);
+
