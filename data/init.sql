ff34d248fcbe4ab2af76ec1ad81a894f7fe5e91b
Aaron <aaronradlicki@arizona.edu>
2024-07-29 17:52:57 -0700

    Added sense table to 'init.sql'

--- data/init.sql
+++ data/init.sql
@@ -2,12 +2,18 @@ CREATE DATABASE demobahasa;
 ALTER DATABASE demobahasa CHARACTER SET utf8 COLLATE utf8_unicode_ci;
 use demobahasa;
 
+CREATE TABLE sense (
+    id INT NOT NULL AUTO_INCREMENT,
+    pos VARCHAR(30),
+    definition VARCHAR(30)
+);
+
 CREATE TABLE lexicon (
     id INT NOT NULL AUTO_INCREMENT,
     written_form NVARCHAR(30),
     origin NVARCHAR(30),
     surface_ipa NVARCHAR(50),
-    senses ????????
+    senses ????????,
     surface_simple NVARCHAR(30)
 );
 
