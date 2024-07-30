20ed13a70d4c68eb4da6a7cb1381a39dc140a948
Aaron <aaronradlicki@arizona.edu>
2024-07-29 18:35:11 -0700

    Completed SQL commands in 'init.sql'

--- data/init.sql
+++ data/init.sql
@@ -2,7 +2,7 @@ CREATE DATABASE demobahasa;
 ALTER DATABASE demobahasa CHARACTER SET utf8 COLLATE utf8_unicode_ci;
 use demobahasa;
 
-CREATE TABLE sense (
+CREATE TABLE senses (
     id INT NOT NULL AUTO_INCREMENT,
     pos ENUM('NOUN','VERB','ADJECTIVE','ADVERB','PREPOSITION','CONJUNCTION','INTERJECTION'),
     definition VARCHAR(30)
@@ -16,4 +16,18 @@ CREATE TABLE lexicon (
     senses VARCHAR(200),
     surface_simple NVARCHAR(30)
 );
+
+INSERT INTO senses
+    (pos, definition)
+VALUES
+    ('ADJECTIVE','male'),
+    ('PREPOSITION','but'),
+    ('VERB','study');
+
+INSERT INTO lexicon
+    (written_form, origin, surface_ipa, senses, surface_simple)
+VALUES
+    ('laki-laki', 'MALAY', 'ˈlakilaki', 1, 'laki-laki'),
+    ('untuk', 'MALAY', 'ˈuntuk', 2, 'untuk'),
+    ('belajar', 'MALAY', 'bəˈladʒar', 3, 'bəlajar')
 
