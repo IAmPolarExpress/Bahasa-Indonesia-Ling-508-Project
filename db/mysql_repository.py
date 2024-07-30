10f33eed2ca8100898e7b90489e9e27a1d4705fd
Aaron <aaronradlicki@arizona.edu>
2024-07-30 13:12:58 -0700

    Updated implementation for senses, etc.

--- db/mysql_repository.py
+++ db/mysql_repository.py
@@ -23,19 +23,25 @@ class MySQLRepository(Repository):
         self.cursor.close()
         self.connection.close()
 
-    def _map_sense(self):
+    def _map_sense(self, sense_number):
         ## Added this since I think sql functions like this will be needed but also
         ## left them unimplemented because I still do not understand how all of the
         ## code for SQL and Python relates to each other.
-        raise NotImplementedError
+        """Pulls a sense from the SQL table based on its number:"""
 
     def _map_lexical_entry(self):
         raise NotImplementedError
 
-    def _mapper(self, entry: dict) -> LexicalEntry:
+    def _lexicon_mapper(self, entry: dict) -> LexicalEntry:
         """Takes a dict of data and turns it into a LexicalEntry class,
-        formulated properly."""
-        raise NotImplementedError
+        formulated properly:"""
+        lexical_entry = LexicalEntry(written_form = entry[written_form],
+                                     origin = entry[origin],
+                                     surface_IPA = entry[surface_IPA],
+                                     senses = [self._map_sense(sense_number) for sense_number in entry(senses)],
+                                     surface_simple = entry[surface_simple])
+        return lexical_entry
+
 
 
     def _load_lexicon(self) -> list[LexicalEntry]:
@@ -47,9 +53,10 @@ class MySQLRepository(Repository):
                     'written_form': form,
                     'origin': origin,
                     'surface_IPA': surface_ipa,
+                    ## Must turn the list of senses into a Python list:
                     'senses': senses,
                     'surface_simple': surface_simple
-                    } for (id, form,   origin, surface_ipa, senses, \
+                    } for (id, form, origin, surface_ipa, senses, \
                            surface_simple) in self.cursor]
-        lexicon = [self._mapper(entry) for entry in entries]
+        lexicon = [self._lexicon_mapper(entry) for entry in entries]
         return lexicon
