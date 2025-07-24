2a05246b7761887a47ce84d00fe886277fc42644
IAmPolarExpress <aaronrad95@gmail.com>
2025-07-24 15:48:02 -0700

    Added further notes to services.py

--- app/services.py
+++ app/services.py
@@ -1,1 +1,6 @@
 ## This is the file that will manage the services layer, once I add that.
+
+## This is where I can implement my use cases, which will in turn call the other data that has been written.
+## I will wait to write code here until after I have reviewed Unit 5.  I think checking out the Week 5 branch of the
+## Sanskrit course demo could help here too:
+## https://github.com/jjberry-508/sanskrit-508/blob/week5/app/services.py
