f5da54260f9e34af467f755cf357f7cb0278a1ed
IAmPolarExpress <aaronrad95@gmail.com>
2025-07-24 17:16:48 -0700

    Services.py 'find_word_origin' method added

--- app/services.py
+++ app/services.py
@@ -4,3 +4,15 @@
 ## I will wait to write code here until after I have reviewed Unit 5.  I think checking out the Week 5 branch of the
 ## Sanskrit course demo could help here too:
 ## https://github.com/jjberry-508/sanskrit-508/blob/week5/app/services.py
+
+import db.mysql_repository
+
+class Services:
+
+    ## Init code shamelessly ripped off from the course Sanskrit code :)
+    def __init__(self):
+        self.repo = db.mysql_repository.MysqlRepository()
+
+    ### USE CASE 4: Returns the origin of the word entered ###
+    def find_word_origin(self, word):
+        raise NotImplementedError
