0ac1eb030d1a58a3866deb2377cec288a68de026
IAmPolarExpress <aaronrad95@gmail.com>
2025-08-08 17:25:29 -0700

    Flask imports on fleek, dawg

--- app/app.py
+++ app/app.py
@@ -1,1 +1,8 @@
 ### This file covers API communication and will be where Flask comes in! :)
+
+## Imports shamelessly stolen from our example code as instructed :)
+from flask import Flask, request, jsonify
+from flask_cors import CORS, cross_origin
+#from logging.config import dictConfig
+
+from app.services import WordServices
