89ecdf4e209b869ebab674b0977b8506a084f517
IAmPolarExpress <aaronrad95@gmail.com>
2025-08-08 17:39:30 -0700

    Further copied code from class project

--- app/app.py
+++ app/app.py
@@ -3,6 +3,47 @@
 ## Imports shamelessly stolen from our example code as instructed :)
 from flask import Flask, request, jsonify
 from flask_cors import CORS, cross_origin
-#from logging.config import dictConfig
+from logging.config import dictConfig
 
-from app.services import WordServices
+## Imports from app.services.py:
+from app.services import *
+
+## More shamelessly ripped off code from the course examples, per official recommendation:
+dictConfig({
+    'version': 1,
+    'formatters': {'default': {
+        'format': '[%(asctime)s] %(levelname)s in %(module)s: %(message)s',
+    }},
+    'handlers': {'wsgi': {
+        'class': 'logging.StreamHandler',
+        'stream': 'ext://flask.logging.wsgi_errors_stream',
+        'formatter': 'default'
+    }},
+    'root': {
+        'level': 'INFO',
+        'handlers': ['wsgi']
+    }
+})
+
+
+app = Flask(__name__)
+app.config['CORS_HEADERS'] = 'Content-Type'
+cors = CORS(app, resources={r"/parse": {"origins": "http://localhost:port"}})
+
+services = Services()
+
+
+@app.route('/')
+def doc() -> str:
+    app.logger.info("doc - Got request")
+    with open("app/doc.html", "r") as f:
+        return f.read()
+
+
+@app.route("/generate", methods=["GET"])
+def generate_words():
+    services.generate_words()
+    app.logger.info("/generate - Generated words.")
+    return jsonify({"msg": "success"})
+
+
